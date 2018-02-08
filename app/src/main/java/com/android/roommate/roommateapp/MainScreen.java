67b9ff323955c620a6d7a3b442a8f6c4d8e458b0
VengefulLimaBean <aguguchev@gmail.com>
2018-02-07 22:02:55 -0500

    cmd test

--- app/src/main/java/com/android/roommate/roommateapp/MainScreen.java
+++ app/src/main/java/com/android/roommate/roommateapp/MainScreen.java
@@ -10,5 +10,6 @@ public class MainScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
         //test changes
+        //git cmd changes
     }
 }
